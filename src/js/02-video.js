ac61b4d501763929a6f6d252185d736df071d056
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-10-03 11:05:08 +0300

    add const KEY

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,10 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 const player = document.querySelector('#vimeo-player');
 const playerVimeo = new Player(player);
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = localStorage.getItem(STORAGE_KEY);
 
 if (currentTime) {
   playerVimeo.setCurrentTime(currentTime);
@@ -12,6 +13,6 @@ if (currentTime) {
 playerVimeo.on('timeupdate', throttle(setCurrentTimeInStorage, 1000));
 
 function setCurrentTimeInStorage(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
   console.log(data.seconds.toFixed(1));
 }
